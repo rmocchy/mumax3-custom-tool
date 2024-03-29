0080506cd0a31de8dc789dafbbd44ae915f25e3d
mozuku55 <ryo.tako.05@gmail.com>
2024-03-29 15:32:18 +0900

    make test ep

--- router.sh
+++ router.sh
@@ -16,8 +16,15 @@ function mx3tool {
     if [ "$1" == "version" ]; then
         echo "current version : $current_version"
     elif [ "$1" == "genmx3" ]; then
-        echo "start"
+        echo "gen start"
         docker run -it --gpus all  --rm -v $PWD:/gen-async mumax3-custom-tool:latest /bin/bash -c "python3 /app/tools/gen-mx3.py && chmod 777 -R /gen-async"
+    elif [ "$1" == "simexec" ]; then
+        echo "sim exec start"
+        # docker run -it --gpus all  --rm -v $PWD:/gen-async mumax3-custom-tool:latest /bin/bash -c "python3 /app/mumax3-executer-main.py exec $(PWD)"
+        docker run -it --gpus all  --rm -v $PWD:/gen-async mumax3-custom-tool:latest /bin/bash -c "python3 /app/mumax3-executer-main.py test $(PWD)"
+    elif [ "$1" == "simtest" ]; then
+        echo "sim test start"
+        docker run -it --gpus all  --rm -v $PWD:/gen-async mumax3-custom-tool:latest /bin/bash -c "python3 /app/mumax3-executer-main.py test $(PWD)"
     else
         echo "invalid command"
         showcommands
